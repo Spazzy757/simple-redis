86671843022c0ed212790ac549fd8b203f032abb
Spazzy <brendankamp757@gmail.com>
2023-01-23 19:46:07 +0100

    chore: initial kubebuilder bootstrap
    Signed-off-by: Spazzy <brendankamp757@gmail.com>

---
+++ api/v1/redis_types.go
@@ -0,0 +1,82 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// RedisSpec defines the desired state of Redis
+type RedisSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// ClusterSize determines the amount of redis instances running
+	ClusterSize string `json:"clusterSize,omitempty"`
+
+	// LogLevel specifies the redis verbosity level.
+	// This can be one of:
+	// debug (a lot of information, useful for development/testing)
+	// verbose (many rarely useful info, but not a mess like the debug level)
+	// notice (moderately verbose, what you want in production probably)
+	// warning (only very important / critical messages are logged)
+	LogLevel string `json:logLevel,omitempty`
+
+	// Set the number of databases. The default database is DB 0, you can select
+	// a different one on a per-connection basis using SELECT <dbid> where
+	// dbid is a number between 0 and 'databases'-1
+	Databases string `json:databases,omitempty`
+}
+
+// RedisStatus defines the observed state of Redis
+type RedisStatus struct {
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// status of redis cluster
+	Status string `json:status,omitempty`
+
+	// master pod name
+	Master string `json:master,omitempty`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// Redis is the Schema for the redis API
+type Redis struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   RedisSpec   `json:"spec,omitempty"`
+	Status RedisStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// RedisList contains a list of Redis
+type RedisList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Redis `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Redis{}, &RedisList{})
+}
